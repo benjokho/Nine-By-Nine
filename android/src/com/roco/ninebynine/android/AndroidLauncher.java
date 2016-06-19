532c3cddd6ce53780c44d419c23dd3a914588e15
Benjo Kho <nb2kho1@gmail.com>
2016-06-20 01:28:55 +0800

    Nine By Nine
    3x3 by 3x3 tictactoe

---
+++ android/src/com/roco/ninebynine/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.roco.ninebynine.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.roco.ninebynine.NineByNine;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NineByNine(), config);
+	}
+}
---
+++ desktop/src/com/roco/ninebynine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.roco.ninebynine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.roco.ninebynine.NineByNine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NineByNine(), config);
+	}
+}
