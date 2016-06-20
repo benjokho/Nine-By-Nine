d8f56fddc8063062be8f7bad8567d89a12abfecd
Benjo Kho <nb2kho1@gmail.com>
2016-06-21 02:06:35 +0800

    added big win condition

--- core/src/com/roco/gameworld/GameWorld.java
+++ core/src/com/roco/gameworld/GameWorld.java
@@ -12,7 +12,8 @@ public class GameWorld {
 	private final int SQUARE_LENGTH = 30;
 	protected int SQUARE_SIDE_COUNT = 9;
 	private Square[][] squares;
-	private Square[][] bigSquares;
+	private Square[][] bigSquaresRep = new Square[3][3];
+	private boolean[][] bigSquaresSettled = new boolean[3][3];
 	private Owner currentPlayer = Owner.PLAYER1;
 	private int[] bigSquareCoor = new int[2];
 	
@@ -30,22 +31,11 @@ public class GameWorld {
 				y = (SQUARE_LENGTH * j) + (tempBigSeparatorY * SEPARATOR_BIG)
 						+ ((j - tempBigSeparatorY) * SEPARATOR_SMALL) + BORDERY;
 				squares[i][j] = new Square(x, y, i, j);
+				
+				bigSquaresRep[tempBigSeparatorX][tempBigSeparatorY] = squares[tempBigSeparatorX * 3][tempBigSeparatorY * 3];
+				bigSquaresSettled[tempBigSeparatorX][tempBigSeparatorY] = false;
 			}
 		}
-
-		bigSquares = new Square[3][3];
-
-		bigSquares[0][0] = squares[0][0];
-		bigSquares[1][0] = squares[3][0];
-		bigSquares[2][0] = squares[6][0];
-
-		bigSquares[0][1] = squares[0][3];
-		bigSquares[1][1] = squares[3][3];
-		bigSquares[2][1] = squares[6][3];
-
-		bigSquares[0][2] = squares[0][6];
-		bigSquares[1][2] = squares[3][6];
-		bigSquares[2][2] = squares[6][6];
 	}
 
 	public void update(float delta) {
@@ -54,8 +44,6 @@ public class GameWorld {
 	}
 
 	public void update(Square square) {
-		// TODO Auto-generated method stub
-		// change square color
 		square.setOwner(currentPlayer);
 		int tempX = square.getCoorX();
 		int tempY = square.getCoorY();
@@ -76,6 +64,7 @@ public class GameWorld {
 		}
 		
 		if (gotThreeInARowSmall(square)) {
+			bigSquaresSettled[bigSquareCoor[0]][bigSquareCoor[1]] = true;
 			for (int i = 0; i < 3; i ++) {
 				for ( int j = 0; j < 3; j++) {
 					squares[(bigSquareCoor[0] * 3) + i][(bigSquareCoor[1] * 3) + j].setOwner(currentPlayer);
@@ -84,6 +73,7 @@ public class GameWorld {
 			}
 			if (gotThreeInARowBig(square)) {
 				// win
+				Gdx.app.log("ALL I DO IS", "WIN");
 			} else {
 				if (true /* all squares are Owner.NEITHER */) {
 					// lose
@@ -118,7 +108,43 @@ public class GameWorld {
 	}
 
 	private boolean gotThreeInARowBig(Square square) {
-		// TODO Auto-generated method stub
+		Owner ownerToCheck = square.getOwner();
+		boolean firstFlag, secondFlag, thirdFlag;
+		
+		//check row
+		firstFlag = bigSquaresRep[0][bigSquareCoor[1]].getOwner() == bigSquaresRep[1][bigSquareCoor[1]].getOwner(); 
+		secondFlag = bigSquaresRep[1][bigSquareCoor[1]].getOwner() == bigSquaresRep[2][bigSquareCoor[1]].getOwner();
+		thirdFlag = bigSquaresSettled[0][bigSquareCoor[1]] && bigSquaresSettled[1][bigSquareCoor[1]] && bigSquaresSettled[2][bigSquareCoor[1]]; 
+		if (firstFlag && secondFlag && thirdFlag) {
+			Gdx.app.log("true", "row");
+			return true;
+		}
+		//check col
+		firstFlag = bigSquaresRep[bigSquareCoor[0]][0].getOwner() == bigSquaresRep[bigSquareCoor[0]][1].getOwner(); 
+		secondFlag = bigSquaresRep[bigSquareCoor[0]][1].getOwner() == bigSquaresRep[bigSquareCoor[0]][2].getOwner();
+		thirdFlag = bigSquaresSettled[bigSquareCoor[0]][0] && bigSquaresSettled[bigSquareCoor[0]][1] && bigSquaresSettled[bigSquareCoor[0]][2]; 
+		if (firstFlag && secondFlag && thirdFlag) {
+			Gdx.app.log("true", "row");
+			return true;
+		}
+		//check if on diag, then check diag
+		if (bigSquareCoor[0] == bigSquareCoor[1]) {
+			firstFlag = bigSquaresRep[0][0].getOwner() == bigSquaresRep[1][1].getOwner(); 
+			secondFlag = bigSquaresRep[1][1].getOwner() == bigSquaresRep[2][2].getOwner();
+			thirdFlag = bigSquaresSettled[0][0] && bigSquaresSettled[1][1] && bigSquaresSettled[2][2]; 
+			if (firstFlag && secondFlag && thirdFlag) {
+				Gdx.app.log("true", "dia");
+				return true;
+			}	
+			
+			firstFlag = bigSquaresRep[2][0].getOwner() == bigSquaresRep[1][1].getOwner(); 
+			secondFlag = bigSquaresRep[1][1].getOwner() == bigSquaresRep[0][2].getOwner();
+			thirdFlag = bigSquaresSettled[2][0] && bigSquaresSettled[1][1] && bigSquaresSettled[0][2]; 
+			if (firstFlag && secondFlag && thirdFlag) {
+				Gdx.app.log("true", "row");
+				return true;
+			}	
+		}
 		return false;
 	}
 
@@ -141,23 +167,23 @@ public class GameWorld {
 			return true;
 		}
 		//check if on diag, then check diag
-		if (tempX == tempY) {
-			firstFlag = squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3)].getOwner() == squares[(bigSquareCoor[0] * 3) + 2][(bigSquareCoor[1] * 3) + 2].getOwner(); 
-			secondFlag = squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3)].getOwner() == squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner();
-			thirdFlag = squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3)].getOwner() == square.getOwner();
-			if (firstFlag && secondFlag && thirdFlag) {
-				Gdx.app.log("true", "dia");
-				return true;
-			}	
+		
+		firstFlag = squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3)].getOwner() == squares[(bigSquareCoor[0] * 3) + 2][(bigSquareCoor[1] * 3) + 2].getOwner(); 
+		secondFlag = squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3)].getOwner() == squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner();
+		thirdFlag = squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3)].getOwner() == square.getOwner();
+		if (firstFlag && secondFlag && thirdFlag) {
+			Gdx.app.log("true", "dia");
+			return true;
+		}	
 			
-			firstFlag = squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner() == squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[0] * 3) + 2].getOwner(); 
-			secondFlag = squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner() == squares[(bigSquareCoor[0] * 3) + 2][(bigSquareCoor[1] * 3)].getOwner();
-			thirdFlag = squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner() == square.getOwner();
-			if (firstFlag && secondFlag && thirdFlag) {
-				Gdx.app.log("true", "row");
-				return true;
-			}	
-		}
+		firstFlag = squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner() == squares[(bigSquareCoor[0] * 3)][(bigSquareCoor[1] * 3) + 2].getOwner(); 
+		secondFlag = squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner() == squares[(bigSquareCoor[0] * 3) + 2][(bigSquareCoor[1] * 3)].getOwner();
+		thirdFlag = squares[(bigSquareCoor[0] * 3) + 1][(bigSquareCoor[1] * 3) + 1].getOwner() == square.getOwner();
+		if (firstFlag && secondFlag && thirdFlag) {
+			Gdx.app.log("true", "row");
+			return true;
+		}	
+		
 		return false;
 	}
 
